3693745b773987374236403aff4ff2352c8f3c1e
Mark Paschal <markpasc@markpasc.org>
2014-03-05 12:00:53 -0500

    Reset User table's pk sequence after bulk creating

--- emailuser/management/commands/converttoemailuser.py
+++ emailuser/management/commands/converttoemailuser.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand, CommandError
+from django.core.management.color import no_style
+from django.db import connections
 
 from emailuser.models import User
 
@@ -18,6 +20,11 @@ class Command(BaseCommand):
                 "convert. Please change the project's AUTH_USER_MODEL "
                 "setting to the previous value so we can find the user data "
                 "to convert.")
+
+        if not user_model.objects.exists():
+            raise CommandError("You have no existing user models to convert. "
+                "Hurrah, you can set AUTH_USER_MODEL and continue without "
+                "converting!")
 
         if user_model.objects.filter(email='').exists():
             raise CommandError("You have existing user models with blank "
@@ -37,6 +44,21 @@ class Command(BaseCommand):
         emailusers = (User(**d) for d in user_data)
         User.objects.all().delete()
         User.objects.bulk_create(emailusers)
+
+        # We bulk created with the same PKs to keep foreign keys intact, but
+        # that may have broken PK generation for that table (for example, if
+        # it's a Postgres database, the table's _id_seq sequence may still
+        # generate 1 next even though we bulk created a User with pk 1).
+        # So reset the sequences.
+        for db_name in connections:
+            conn = connections[db_name]
+            sql = conn.ops.sequence_reset_sql(no_style(), [User])
+            cursor = conn.cursor()
+            try:
+                for line in sql:
+                    cursor.execute(line)
+            finally:
+                cursor.close()
 
         # TODO: swap our ContentType for the original user_model's to preserve generic foreign key references?
 
